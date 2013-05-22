6f327b6036d090543fc0709731899a89abe900c8
Griffin <griff.college@gmail.com>
2013-05-21 21:33:11 -0400

    TitleScreen

--- Helicopter Hysteria/Helicopter Hysteria/Game1.cs
+++ Helicopter Hysteria/Helicopter Hysteria/Game1.cs
@@ -13,30 +13,32 @@ using Helicopter_Hysteria.States;
 
 namespace Helicopter_Hysteria
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
+    
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        public static int GAME_WIDTH = 1280;
+        public static  int GAME_HEIGHT = 720;
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        public SpriteBatch spriteBatch;
         GameStateManager manager;
+        InputHandler input;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            graphics.PreferredBackBufferHeight = GAME_HEIGHT;
+            graphics.PreferredBackBufferWidth = GAME_WIDTH;
+            graphics.ApplyChanges();
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+
+            input = new InputHandler(this);
+            Components.Add(input);
             manager = new GameStateManager(this);
             Components.Add(manager);
             manager.ChangeState(new TitleState(this, manager));
@@ -44,52 +46,26 @@ namespace Helicopter_Hysteria
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
+
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
         }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
-        }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        }
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
--- Helicopter Hysteria/Helicopter Hysteria/States/TitleState.cs
+++ Helicopter Hysteria/Helicopter Hysteria/States/TitleState.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using GameHelperLibrary;
 
 namespace Helicopter_Hysteria.States
 {
     public class TitleState : BaseGameState
     {
+        public Texture2D titleScreen;
+        public Rectangle titleScreenRec;
+        public Vector2 origin;
+       
         public TitleState(Game game, GameStateManager manager)
             : base(game, manager)
-        { }
+        {
+            origin = new Vector2(0, 0);
+            titleScreenRec = new Rectangle(0, 0, Game1.GAME_WIDTH, Game1.GAME_HEIGHT);
+        }
 
         public override void Initialize()
         {
@@ -20,16 +32,25 @@ namespace Helicopter_Hysteria.States
 
         protected override void LoadContent()
         {
+            var content = gameRef.Content;
+            titleScreen = content.Load<Texture2D>("Titlescreen");
+
             base.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
+           
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
+            var spriteBatch = gameRef.spriteBatch;
+            spriteBatch.Begin();
+            spriteBatch.Draw(titleScreen, titleScreenRec, Color.White);
+            spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
